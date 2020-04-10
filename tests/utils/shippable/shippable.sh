41562a0c223810b79d7a6985d54794048ca9019d
Felix Fontein <felix@fontein.de>
2020-04-10 15:44:42 +0200

    Update testing (#26)
    * Remove superfluous requirements file.
    
    * Update RHEL 7.6 to RHEL 7.8 in CI.
    
    * Use stable-2.9 branch instead of latest released 2.9 version.
    
    * Remove ignore.txt entries.
    
    ci_complete

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -55,7 +55,8 @@ pip list --disable-pip-version-check
 if [ "${ansible_version}" == "devel" ]; then
     retry pip install https://github.com/ansible/ansible/archive/devel.tar.gz --disable-pip-version-check
 else
-    retry pip install ansible==${ansible_version} --disable-pip-version-check
+    retry pip install https://github.com/ansible/ansible/archive/stable-2.9.tar.gz --disable-pip-version-check
+    # retry pip install ansible==${ansible_version} --disable-pip-version-check
     # force complete CI run for Ansible 2.9: 2.9's ansible-test's change detection for collections is broken
     COMPLETE=yes
 fi
