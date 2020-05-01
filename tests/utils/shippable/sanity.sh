afb2f4287b1220284abc2a163ffbf11827ee1f6b
Felix Fontein <felix@fontein.de>
2020-05-01 11:53:21 +0200

    Avoid sanity excludes so that users can run  ansible-test sanity --docker  and it usually passes.

--- tests/utils/shippable/sanity.sh
+++ tests/utils/shippable/sanity.sh
@@ -2,11 +2,6 @@
 
 set -o pipefail -eux
 
-declare -a args
-IFS='/:' read -ra args <<< "$1"
-
-group="${args[1]}"
-
 if [ "${BASE_BRANCH:-}" ]; then
     base_branch="origin/${BASE_BRANCH}"
 else
@@ -16,5 +11,4 @@ fi
 # shellcheck disable=SC2086
 ansible-test sanity --color -v --junit ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
     --docker --base-branch "${base_branch}" \
-    --exclude shippable.yml --exclude tests/utils/ \
     --allow-disabled
