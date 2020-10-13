36b68101f7630a07f5dd3cc9e4b29a498ff063bf
Felix Fontein <felix@fontein.de>
2020-10-13 13:44:11 +0200

    Mention no_log for openssl_privatekey's return_content option.

--- plugins/modules/openssl_privatekey.py
+++ plugins/modules/openssl_privatekey.py
@@ -152,6 +152,8 @@ options:
             - If set to C(yes), will return the (current or generated) private key's content as I(privatekey).
             - Note that especially if the private key is not encrypted, you have to make sure that the returned
               value is treated appropriately and not accidentally written to logs etc.! Use with care!
+            - Use Ansible's I(no_log) task option to avoid the output being shown. See also
+              U(https://docs.ansible.com/ansible/latest/reference_appendices/faq.html#how-do-i-keep-secret-data-in-my-playbook).
         type: bool
         default: no
         version_added: '1.0.0'
