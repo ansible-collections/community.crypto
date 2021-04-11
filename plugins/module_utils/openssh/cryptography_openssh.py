988173989a4d392ea10ae31b7b850b20bb56e327
Andrew Pantuso <ajpantuso@gmail.com>
2021-04-11 11:28:24 -0400

    Refactored loading/generation for Asym keypairs into classmethods

--- plugins/module_utils/openssh/cryptography_openssh.py
+++ plugins/module_utils/openssh/cryptography_openssh.py
@@ -36,7 +36,8 @@ try:
     from cryptography.exceptions import InvalidSignature, UnsupportedAlgorithm
     from cryptography.hazmat.backends.openssl import backend
     from cryptography.hazmat.primitives import hashes, serialization
-    from cryptography.hazmat.primitives.asymmetric import dsa, ec, rsa, padding
+    from cryptography.hazmat.primitives.asymmetric import dsa, ec, rsa
+    from cryptography.hazmat.primitives.asymmetric import padding
     from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 except ImportError:
     pass
@@ -87,80 +88,237 @@ class InvalidPassphraseError(OpenSSHError):
 class Asymmetric_Keypair(object):
     """Container for newly generated asymmetric keypairs or those loaded from existing files"""
 
-    def __init__(self, path=None, keytype='rsa', size=None, passphrase=None, key_format='PEM'):
-        """
-           :path: A path to an existing private key to be loaded
-           :keytype: One of rsa, dsa, ecdsa, ed25519
-           :size: The key length for newly generated keys
-           :passphrase: Secret used to decrypt the private key being loaded
-            or encrypt the private key being generated
-           :key_format: Format of key files being loaded
-        """
-
-        self.__algorithm_parameters = {
-            'rsa': {
-                'default_size': 2048,
-                'valid_sizes': range(1024, 16384),
-                'signer_params': {
-                    'padding': padding.PSS(
-                        mgf=padding.MGF1(hashes.SHA256()),
-                        salt_length=padding.PSS.MAX_LENGTH,
-                    ),
-                    'algorithm': hashes.SHA256(),
-                },
+    __algorithm_parameters = {
+        'rsa': {
+            'default_size': 2048,
+            'valid_sizes': range(1024, 16384),
+            'signer_params': {
+                'padding': padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH,
+                ),
+                'algorithm': hashes.SHA256(),
             },
-            'dsa': {
-                'default_size': 1024,
-                'valid_sizes': [1024],
-                'signer_params': {
-                    'algorithm': hashes.SHA256(),
-                },
+        },
+        'dsa': {
+            'default_size': 1024,
+            'valid_sizes': [1024],
+            'signer_params': {
+                'algorithm': hashes.SHA256(),
             },
-            'ed25519': {
-                'default_size': 256,
-                'valid_sizes': [256],
-                'signer_params': {},
+        },
+        'ed25519': {
+            'default_size': 256,
+            'valid_sizes': [256],
+            'signer_params': {},
+        },
+        'ecdsa': {
+            'default_size': 256,
+            'valid_sizes': [256, 384, 521],
+            'signer_params': {
+                'signature_algorithm': ec.ECDSA(hashes.SHA256()),
             },
-            'ecdsa': {
-                'default_size': 256,
-                'valid_sizes': [256, 384, 521],
-                'signer_params': {
-                    'signature_algorithm': ec.ECDSA(hashes.SHA256()),
-                },
-                'curves': {
-                    256: ec.SECP256R1(),
-                    384: ec.SECP384R1(),
-                    521: ec.SECP521R1(),
-                }
+            'curves': {
+                256: ec.SECP256R1(),
+                384: ec.SECP384R1(),
+                521: ec.SECP521R1(),
             }
         }
+    }
 
-        self.__encryption_algorithm = serialization.NoEncryption()
+    @classmethod
+    def __validate_passphrase(cls, passphrase):
+        if not hasattr(passphrase, 'encode'):
+            raise InvalidPassphraseError("%s cannot be encoded to text" % passphrase)
+
+    @classmethod
+    def generate(cls, keytype='rsa', size=None, passphrase=None):
+        """Returns an Asymmetric_Keypair object generated with the supplied parameters or defaults to an unencrypted RSA-2048 key
+
+           :keytype: One of rsa, dsa, ecdsa, ed25519
+           :size: The key length for newly generated keys
+           :passphrase: Secret used to encrypt the private key being generated
+        """
+
+        if keytype not in cls.__algorithm_parameters.keys():
+            raise InvalidKeyTypeError("%s is not a valid keytype. Valid keytypes are %s" % (keytype, ", ".join(cls.__algorithm_parameters.keys())))
+
+        if not size:
+            size = cls.__algorithm_parameters[keytype]['default_size']
+        else:
+            if size not in cls.__algorithm_parameters[keytype]['valid_sizes']:
+                raise InvalidKeySizeError("%s is not a valid key size for %s keys" % (size, keytype))
 
         if passphrase:
-            self.update_passphrase(passphrase)
-
-        if path:
-            self.__keytype = None
-            self.__privatekey = self.__load_privatekey(path, passphrase, key_format)
-            self.__publickey = self.__load_publickey(path, key_format)
-            self.__size = self.__privatekey.key_size
-
-            if not self.verify(self.sign(b'message'), b'message'):
-                raise InvalidPublicKeyFileError("%s.pub is not the correct public key for %s" % (path, path))
+            cls.__validate_passphrase(passphrase)
+            encryption_algorithm = serialization.BestAvailableEncryption(passphrase.encode(encoding=_TEXT_ENCODING))
         else:
-            if keytype not in self.__algorithm_parameters.keys():
-                raise InvalidKeyTypeError("%s is not a valid keytype. Valid keytypes are %s" % (keytype, ", ".join(self.__algorithm_parameters.keys())))
+            encryption_algorithm = serialization.NoEncryption()
 
-            self.__keytype = keytype
+        if keytype == 'rsa':
+            privatekey = rsa.generate_private_key(
+                # Public exponent should always be 65537 to prevent issues
+                # if improper padding is used during signing
+                public_exponent=65537,
+                key_size=size,
+                backend=backend,
+            )
+        elif keytype == 'dsa':
+            privatekey = dsa.generate_private_key(
+                key_size=size,
+                backend=backend,
+            )
+        elif keytype == 'ed25519':
+            privatekey = Ed25519PrivateKey.generate()
+        elif keytype == 'ecdsa':
+            privatekey = ec.generate_private_key(
+                cls.__algorithm_parameters['ecdsa']['curves'][size],
+                backend=backend,
+            )
 
-            if size:
-                self.__size = size
-            else:
-                self.__size = self.__algorithm_parameters[self.__keytype]['default_size']
+        publickey = privatekey.public_key()
 
-            self.__privatekey = self.__generate_privatekey()
-            self.__publickey = self.__generate_publickey()
+        return cls(
+            keytype=keytype,
+            size=size,
+            privatekey=privatekey,
+            publickey=publickey,
+            encryption_algorithm=encryption_algorithm
+        )
+
+    @classmethod
+    def load(cls, path, passphrase=None, key_format='PEM'):
+        """Returns an Asymmetric_Keypair object loaded from the supplied file path
+
+           :path: A path to an existing private key to be loaded
+           :passphrase: Secret used to decrypt the private key being loaded
+           :key_format: Format of key files to be loaded
+        """
+
+        if passphrase:
+            cls.__validate_passphrase(passphrase)
+            encryption_algorithm = serialization.BestAvailableEncryption(passphrase.encode(encoding=_TEXT_ENCODING))
+        else:
+            encryption_algorithm = serialization.NoEncryption()
+
+        privatekey = cls.__load_privatekey(path, passphrase, key_format)
+        publickey = cls.__load_publickey(path, key_format)
+        size = privatekey.key_size
+
+        if isinstance(privatekey, rsa.RSAPrivateKey):
+            keytype = 'rsa'
+        elif isinstance(privatekey, dsa.DSAPrivateKey):
+            keytype = 'dsa'
+        elif isinstance(privatekey, ec.EllipticCurvePrivateKey):
+            keytype = 'ecdsa'
+        elif isinstance(privatekey, Ed25519PrivateKey):
+            keytype = 'ed25519'
+
+        return cls(
+            keytype=keytype,
+            size=size,
+            privatekey=privatekey,
+            publickey=publickey,
+            encryption_algorithm=encryption_algorithm
+        )
+
+    @classmethod
+    def __load_privatekey(cls, path, passphrase, key_format):
+        privatekey_loaders = {
+            'PEM': serialization.load_pem_private_key,
+            'DER': serialization.load_der_private_key,
+        }
+
+        # OpenSSH formatted private keys are not available in Cryptography <3.0
+        if hasattr(serialization, 'load_ssh_private_key'):
+            privatekey_loaders['SSH'] = serialization.load_ssh_private_key
+        else:
+            privatekey_loaders['SSH'] = serialization.load_pem_private_key
+
+        try:
+            privatekey_loader = privatekey_loaders[key_format]
+        except KeyError:
+            raise InvalidKeyFormatError(
+                "%s is not a valid key format (%s)" % (
+                    key_format,
+                    ','.join(privatekey_loaders.keys())
+                )
+            )
+
+        if passphrase:
+            passphrase = passphrase.encode(encoding=_TEXT_ENCODING)
+
+        try:
+            with open(path, 'rb') as f:
+                content = f.read()
+
+                privatekey = privatekey_loader(
+                    data=content,
+                    password=passphrase,
+                    backend=backend,
+                )
+
+        except ValueError as e:
+            raise InvalidPrivateKeyFileError(e)
+        except TypeError as e:
+            raise InvalidPassphraseError(e)
+        except UnsupportedAlgorithm as e:
+            raise InvalidAlgorithmError(e)
+
+        return privatekey
+
+    @classmethod
+    def __load_publickey(cls, path, key_format):
+        publickey_loaders = {
+            'PEM': serialization.load_pem_public_key,
+            'DER': serialization.load_der_public_key,
+            'SSH': serialization.load_ssh_public_key,
+        }
+
+        try:
+            publickey_loader = publickey_loaders[key_format]
+        except KeyError:
+            raise InvalidKeyFormatError(
+                "%s is not a valid key format (%s)" % (
+                    key_format,
+                    ','.join(publickey_loaders.keys())
+                )
+            )
+
+        try:
+            with open(path + '.pub', 'rb') as f:
+                content = f.read()
+
+                publickey = publickey_loader(
+                    data=content,
+                    backend=backend,
+                )
+        except ValueError as e:
+            raise InvalidPublicKeyFileError(e)
+        except UnsupportedAlgorithm as e:
+            raise InvalidAlgorithmError(e)
+
+        return publickey
+
+    def __init__(self, keytype, size, privatekey, publickey, encryption_algorithm):
+        """
+           :keytype: One of rsa, dsa, ecdsa, ed25519
+           :size: The key length for the private key of this key pair
+           :privatekey: Private key object of this key pair
+           :publickey: Public key object of this key pair
+           :encryption_algorithm: Hashed secret used to encrypt the private key of this key pair
+        """
+
+        self.__size = size
+        self.__keytype = keytype
+        self.__privatekey = privatekey
+        self.__publickey = publickey
+        self.__encryption_algorithm = encryption_algorithm
+
+        if not self.verify(self.sign(b'message'), b'message'):
+            raise InvalidPublicKeyFileError(
+                "The private key and public key of this keypair do not match"
+            )
 
     @property
     def private_key(self):
@@ -201,7 +359,7 @@ class Asymmetric_Keypair(object):
         try:
             signature = self.__privatekey.sign(
                 data,
-                **self.__algorithm_parameters[self.__keytype]['signer_params']
+                **type(self).__algorithm_parameters[self.__keytype]['signer_params']
             )
         except TypeError as e:
             raise InvalidDataError(e)
@@ -220,177 +378,24 @@ class Asymmetric_Keypair(object):
             self.__publickey.verify(
                 signature,
                 data,
-                **self.__algorithm_parameters[self.__keytype]['signer_params']
+                **type(self).__algorithm_parameters[self.__keytype]['signer_params']
             )
         except InvalidSignature:
             return False
 
         return True
 
-    def update_passphrase(self, passphrase):
+    def update_passphrase(self, passphrase=None):
         """Updates the encryption algorithm of this key pair
 
            :passphrase: Text secret used to encrypt this key pair
         """
 
-        self.__validate_passphrase(passphrase)
-        self.__encryption_algorithm = serialization.BestAvailableEncryption(passphrase.encode(encoding=_TEXT_ENCODING))
-
-    @classmethod
-    def generate(cls, keytype='rsa', size=None, passphrase=None):
-        """Returns an Asymmetric_Keypair object generated with the supplied parameters or defaults to an unencrypted RSA-2048 key
-
-           :keytype: One of rsa, dsa, ecdsa, ed25519
-           :size: The key length for newly generated keys
-           :passphrase: Secret used to encrypt the private key being generated
-        """
-
-        return cls(keytype=keytype, size=size, passphrase=passphrase)
-
-    @classmethod
-    def load(cls, path, passphrase=None, key_format='PEM'):
-        """Returns an Asymmetric_Keypair object loaded from the supplied file path
-
-           :path: A path to an existing private key to be loaded
-           :passphrase: Secret used to decrypt the private key being loaded
-           :key_format: Format of key files to be loaded
-        """
-
-        return cls(path=path, passphrase=passphrase, key_format=key_format)
-
-    def __generate_privatekey(self):
-
-        generators = {
-            'rsa': self.__generate_rsa_key,
-            'dsa': self.__generate_dsa_key,
-            'ed25519': self.__generate_ed25519_key,
-            'ecdsa': self.__generate_ecdsa_key,
-        }
-
-        self.__validate_key_size()
-
-        return generators[self.__keytype]()
-
-    def __generate_publickey(self):
-
-        return self.__privatekey.public_key()
-
-    def __validate_key_size(self):
-
-        if self.__size not in self.__algorithm_parameters[self.__keytype]['valid_sizes']:
-            raise InvalidKeySizeError("%s is not a valid key size for %s keys" % (self.__size, self.__keytype))
-
-    def __generate_rsa_key(self):
-        return rsa.generate_private_key(
-            # Public exponent should always be 65537 to prevent issues
-            # if improper padding is used during signing
-            public_exponent=65537,
-            key_size=self.__size,
-            backend=backend,
-        )
-
-    def __generate_dsa_key(self):
-        return dsa.generate_private_key(
-            key_size=self.__size,
-            backend=backend,
-        )
-
-    def __generate_ed25519_key(self):
-        return Ed25519PrivateKey.generate()
-
-    def __generate_ecdsa_key(self):
-        return ec.generate_private_key(
-            self.__algorithm_parameters['ecdsa']['curves'][self.__size],
-            backend=backend,
-        )
-
-    def __load_privatekey(self, path, passphrase, key_format):
-        privatekey_loaders = {
-            'PEM': serialization.load_pem_private_key,
-            'DER': serialization.load_der_private_key,
-        }
-
-        # OpenSSH formatted private keys are not available in Cryptography <3.0
-        if hasattr(serialization, 'load_ssh_private_key'):
-            privatekey_loaders['SSH'] = serialization.load_ssh_private_key
-        else:
-            privatekey_loaders['SSH'] = serialization.load_pem_private_key
-
-        try:
-            privatekey_loader = privatekey_loaders[key_format]
-        except KeyError:
-            raise InvalidKeyFormatError(
-                "%s is not a valid key format (%s)" % (
-                    key_format,
-                    ','.join(privatekey_loaders.keys())
-                )
-            )
-
         if passphrase:
-            passphrase = passphrase.encode(encoding=_TEXT_ENCODING)
-
-        try:
-            with open(path, 'rb') as f:
-                content = f.read()
-
-                privatekey = privatekey_loader(
-                    data=content,
-                    password=passphrase,
-                    backend=backend,
-                )
-
-                if isinstance(privatekey, rsa.RSAPrivateKey):
-                    self.__keytype = 'rsa'
-                elif isinstance(privatekey, dsa.DSAPrivateKey):
-                    self.__keytype = 'dsa'
-                elif isinstance(privatekey, ec.EllipticCurvePrivateKey):
-                    self.__keytype = 'ecdsa'
-                elif isinstance(privatekey, Ed25519PrivateKey):
-                    self.__keytype = 'ed25519'
-        except ValueError as e:
-            raise InvalidPrivateKeyFileError(e)
-        except TypeError as e:
-            raise InvalidPassphraseError(e)
-        except UnsupportedAlgorithm as e:
-            raise InvalidAlgorithmError(e)
-
-        return privatekey
-
-    def __load_publickey(self, path, key_format):
-        publickey_loaders = {
-            'PEM': serialization.load_pem_public_key,
-            'DER': serialization.load_der_public_key,
-            'SSH': serialization.load_ssh_public_key,
-        }
-
-        try:
-            publickey_loader = publickey_loaders[key_format]
-        except KeyError:
-            raise InvalidKeyFormatError(
-                "%s is not a valid key format (%s)" % (
-                    key_format,
-                    ','.join(publickey_loaders.keys())
-                )
-            )
-
-        try:
-            with open(path + '.pub', 'rb') as f:
-                content = f.read()
-
-                publickey = publickey_loader(
-                    data=content,
-                    backend=backend,
-                )
-        except ValueError as e:
-            raise InvalidPublicKeyFileError(e)
-        except UnsupportedAlgorithm as e:
-            raise InvalidAlgorithmError(e)
-
-        return publickey
-
-    def __validate_passphrase(self, passphrase):
-        if not hasattr(passphrase, 'encode'):
-            raise InvalidPassphraseError("%s cannot be encoded to text" % passphrase)
+            self.__validate_passphrase(passphrase)
+            self.__encryption_algorithm = serialization.BestAvailableEncryption(passphrase.encode(encoding=_TEXT_ENCODING))
+        else:
+            self.__encryption_algorithm = serialization.NoEncryption()
 
 
 class OpenSSH_Keypair(object):
