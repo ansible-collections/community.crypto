417f2a9f9a70fd4535e54f31605e1aecda94a2f1
Andrew Pantuso <ajpantuso@gmail.com>
2021-06-09 22:52:21 +0000

    Initial commit

--- plugins/module_utils/openssh/utils.py
+++ plugins/module_utils/openssh/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# (c) 2020, Doug Stanley <doug+ansible@technologixllc.com>
+# Copyright: (c) 2020, Doug Stanley <doug+ansible@technologixllc.com>
+# Copyright: (c) 2021, Andrew Pantuso (@ajpantuso) <ajpantuso@gmail.com>
 #
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,6 +20,38 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import re
+from struct import Struct
+
+from ansible.module_utils.six import PY3
+
+# Protocol References
+# -------------------
+# https://datatracker.ietf.org/doc/html/rfc4251
+# https://datatracker.ietf.org/doc/html/rfc4253
+# https://datatracker.ietf.org/doc/html/rfc5656
+# https://datatracker.ietf.org/doc/html/rfc8032
+#
+# Inspired by:
+# ------------
+# https://github.com/pyca/cryptography/blob/main/src/cryptography/hazmat/primitives/serialization/ssh.py
+# https://github.com/paramiko/paramiko/blob/master/paramiko/message.py
+
+if PY3:
+    long = int
+
+# 0 (False) or 1 (True) encoded as a single byte
+_BOOLEAN = Struct(b'?')
+# Unsigned 8-bit integer in network-byte-order
+_UBYTE = Struct(b'!B')
+_UBYTE_MAX = 0xFF
+# Unsigned 32-bit integer in network-byte-order
+_UINT32 = Struct(b'!I')
+# Unsigned 32-bit little endian integer
+_UINT32_LE = Struct(b'<I')
+_UINT32_MAX = 0xFFFFFFFF
+# Unsigned 64-bit integer in network-byte-order
+_UINT64 = Struct(b'!Q')
+_UINT64_MAX = 0xFFFFFFFFFFFFFFFF
 
 
 def parse_openssh_version(version_string):
@@ -33,3 +66,286 @@ def parse_openssh_version(version_string):
         version = None
 
     return version
+
+
+# See https://datatracker.ietf.org/doc/html/rfc4251#section-5 for SSH data types
+class OpensshParser(object):
+    """Parser for OpenSSH encoded objects"""
+    BOOLEAN_OFFSET = 1
+    UINT32_OFFSET = 4
+    UINT64_OFFSET = 8
+
+    def __init__(self, data):
+        if not isinstance(data, (bytes, bytearray)):
+            raise TypeError("Data must be bytes-like not %s" % type(data))
+
+        self._data = memoryview(data) if PY3 else data
+        self._pos = 0
+
+    def boolean(self):
+        self._check_position(self.BOOLEAN_OFFSET)
+
+        next_pos = self._pos + self.BOOLEAN_OFFSET
+        value = _BOOLEAN.unpack(self._data[self._pos:next_pos])[0]
+        self._pos = next_pos
+        return value
+
+    def uint32(self):
+        self._check_position(self.UINT32_OFFSET)
+
+        next_pos = self._pos + self.UINT32_OFFSET
+        value = _UINT32.unpack(self._data[self._pos:next_pos])[0]
+        self._pos = next_pos
+        return value
+
+    def uint64(self):
+        self._check_position(self.UINT64_OFFSET)
+
+        next_pos = self._pos + self.UINT64_OFFSET
+        value = _UINT64.unpack(self._data[self._pos:next_pos])[0]
+        self._pos = next_pos
+        return value
+
+    def string(self):
+        length = self.uint32()
+        self._check_position(length)
+
+        next_pos = self._pos + length
+        value = self._data[self._pos:next_pos]
+        self._pos = next_pos
+        # Cast to bytes is required as a memoryview slice is itself a memoryview
+        return value if not PY3 else bytes(value)
+
+    def mpint(self):
+        return self._big_int(self.string(), "big", signed=True)
+
+    def name_list(self):
+        raw_string = self.string()
+        return raw_string.decode('ASCII').split(',')
+
+    def string_list(self):
+        result = []
+        raw_string = self.string()
+
+        if raw_string:
+            parser = OpensshParser(raw_string)
+            while parser.remaining_bytes():
+                result.append(parser.string())
+
+        return result
+
+    def option_list(self):
+        result = []
+        raw_string = self.string()
+
+        if raw_string:
+            parser = OpensshParser(raw_string)
+
+            while parser.remaining_bytes():
+                name = parser.string()
+                data = parser.string()
+                if data:
+                    # data is doubly-encoded
+                    data = OpensshParser(data).string()
+                result.append((name, data))
+
+        return result
+
+    def seek(self, offset):
+        self._check_position(offset)
+        self._pos = self._pos + offset
+
+        return self._pos
+
+    def remaining_bytes(self):
+        return len(self._data) - self._pos
+
+    def _check_position(self, offset):
+        if self._pos + offset > len(self._data):
+            raise ValueError("Insufficient data remaining at position: %s" % self._pos)
+        elif self._pos + offset < 0:
+            raise ValueError("Position cannot be less than zero.")
+        else:
+            return
+
+    @classmethod
+    def signature_data(cls, signature_string, key_type):
+        signature_data = {}
+        if key_type == 'rsa':
+            # https://datatracker.ietf.org/doc/html/rfc4253#section-6.6
+            signature_data['s'] = cls._big_int(signature_string, "big")
+        elif key_type == 'dsa':
+            # https://datatracker.ietf.org/doc/html/rfc4253#section-6.6
+            signature_data['r'] = cls._big_int(signature_string[:20], "big")
+            signature_data['s'] = cls._big_int(signature_string[20:], "big")
+        elif key_type in ('ecdsa-nistp256', 'ecdsa-nistp384', 'ecdsa-nistp521'):
+            parser = cls(signature_string)
+            # https://datatracker.ietf.org/doc/html/rfc5656#section-3.1.2
+            signature_data['r'] = parser.mpint()
+            signature_data['s'] = parser.mpint()
+        elif key_type == 'ed25519':
+            # https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.2
+            signature_data['R'] = cls._big_int(signature_string[:32], "little")
+            signature_data['S'] = cls._big_int(signature_string[32:], "little")
+        else:
+            raise ValueError("%s is not a valid key type" % key_type)
+
+        return signature_data
+
+    @classmethod
+    def _big_int(cls, raw_string, byte_order, signed=False):
+        if byte_order not in ("big", "little"):
+            raise ValueError("Byte_order must be one of (big, little) not %s" % byte_order)
+
+        if PY3:
+            return int.from_bytes(raw_string, byte_order, signed=signed)
+
+        result = 0
+        byte_length = len(raw_string)
+
+        if byte_length > 0:
+            # Check sign-bit
+            msb = raw_string[0] if byte_order == "big" else raw_string[-1]
+            negative = bool(ord(msb) & 0x80)
+            # Match pad value for two's complement
+            pad = b'\xFF' if signed and negative else b'\x00'
+            # The definition of ``mpint`` enforces that unnecessary bytes are not encoded so they are added back
+            pad_length = (4 - byte_length % 4)
+            if pad_length < 4:
+                raw_string = pad * pad_length + raw_string if byte_order == "big" else raw_string + pad * pad_length
+                byte_length += pad_length
+            # Accumulate arbitrary precision integer bytes in the appropriate order
+            if byte_order == "big":
+                for i in range(0, byte_length, cls.UINT32_OFFSET):
+                    left_shift = result << cls.UINT32_OFFSET * 8
+                    result = left_shift + _UINT32.unpack(raw_string[i:i + cls.UINT32_OFFSET])[0]
+            else:
+                for i in range(byte_length, 0, -cls.UINT32_OFFSET):
+                    left_shift = result << cls.UINT32_OFFSET * 8
+                    result = left_shift + _UINT32_LE.unpack(raw_string[i - cls.UINT32_OFFSET:i])[0]
+            # Adjust for two's complement
+            if signed and negative:
+                result -= 1 << (8 * byte_length)
+
+        return result
+
+
+class OpensshWriter(object):
+    """Writes SSH encoded values to a bytes-like buffer"""
+    def __init__(self, buffer=None):
+        if buffer is not None:
+            if not isinstance(buffer, (bytes, bytearray)):
+                raise TypeError("Buffer must be a bytes-like object not %s" % type(buffer))
+        else:
+            buffer = bytearray()
+
+        self._buff = buffer
+
+    def boolean(self, value):
+        if not isinstance(value, bool):
+            raise TypeError("Value must be of type bool not %s" % type(value))
+
+        self._buff.extend(_BOOLEAN.pack(value))
+
+    def uint32(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be of type int not %s" % type(value))
+        if value < 0 or value > _UINT32_MAX:
+            raise ValueError("Value must be a positive integer less than %s" % _UINT32_MAX)
+
+        self._buff.extend(_UINT32.pack(value))
+
+    def uint64(self, value):
+        if not isinstance(value, (long, int)):
+            raise TypeError("Value must be of type (long, int) not %s" % type(value))
+        if value < 0 or value > _UINT64_MAX:
+            raise ValueError("Value must be a positive integer less than %s" % _UINT64_MAX)
+
+        self._buff.extend(_UINT64.pack(value))
+
+    def string(self, value):
+        if not isinstance(value, (bytes, bytearray)):
+            raise TypeError("Value must be bytes-like not %s" % type(value))
+        self.uint32(len(value))
+        self._buff.extend(value)
+
+    def mpint(self, value):
+        if not isinstance(value, (int, long)):
+            raise TypeError("Value must be of type (long, int) not %s" % type(value))
+
+        self.string(self._int_to_mpint(value))
+
+    def name_list(self, value):
+        if not isinstance(value, list):
+            raise TypeError("Value must be a list of byte strings not %s" % type(value))
+
+        try:
+            self.string(','.join(value).encode('ASCII'))
+        except UnicodeEncodeError as e:
+            raise ValueError("Name-list's must consist of US-ASCII characters: %s" % e)
+
+    def string_list(self, value):
+        if not isinstance(value, list):
+            raise TypeError("Value must be a list of byte string not %s" % type(value))
+
+        writer = OpensshWriter()
+        for s in value:
+            writer.string(s)
+
+        self.string(writer.bytes())
+
+    def option_list(self, value):
+        if not isinstance(value, list) or (value and not isinstance(value[0], tuple)):
+            raise TypeError("Value must be a list of tuples")
+
+        writer = OpensshWriter()
+        for pair in value:
+            writer.string(pair[0])
+            # SSH option data is encoded twice though this behavior is not documented
+            inner_writer = OpensshWriter()
+            inner_writer.string(pair[1])
+            writer.string(inner_writer.bytes())
+
+        self.string(writer.bytes())
+
+    @staticmethod
+    def _int_to_mpint(num):
+        if PY3:
+            byte_length = (num.bit_length() + 7) // 8
+            try:
+                result = num.to_bytes(byte_length, "big", signed=True)
+            # Handles values which require \x00 or \xFF to pad sign-bit
+            except OverflowError:
+                result = num.to_bytes(byte_length + 1, "big", signed=True)
+        else:
+            result = bytes()
+            # 0 and -1 are treated as special cases since they are used as sentinels for all other values
+            if num == 0:
+                result += b'\x00'
+            elif num == -1:
+                result += b'\xFF'
+            elif num > 0:
+                while num >> 32:
+                    result = _UINT32.pack(num & _UINT32_MAX) + result
+                    num = num >> 32
+                # Pack last 4 bytes individually to discard insignificant bytes
+                while num:
+                    result = _UBYTE.pack(num & _UBYTE_MAX) + result
+                    num = num >> 8
+                # Zero pad final byte if most-significant bit is 1 as per mpint definition
+                if ord(result[0]) & 0x80:
+                    result = b'\x00' + result
+            else:
+                while (num >> 32) < -1:
+                    result = _UINT32.pack(num & _UINT32_MAX) + result
+                    num = num >> 32
+                while num < -1:
+                    result = _UBYTE.pack(num & _UBYTE_MAX) + result
+                    num = num >> 8
+                if not ord(result[0]) & 0x80:
+                    result = b'\xFF' + result
+
+        return result
+
+    def bytes(self):
+        return bytes(self._buff)
---
+++ tests/unit/plugins/module_utils/openssh/test_utils.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.community.crypto.plugins.module_utils.openssh.utils import (
+    parse_openssh_version,
+    OpensshParser,
+    OpensshWriter
+)
+
+SSH_VERSION_STRING = "OpenSSH_7.9p1, OpenSSL 1.1.0i-fips  14 Aug 2018"
+SSH_VERSION_NUMBER = "7.9"
+
+VALID_BOOLEAN = [
+    True,
+    False
+]
+INVALID_BOOLEAN = [
+    0x02
+]
+VALID_UINT32 = [
+    0x00,
+    0x01,
+    0x01234567,
+    0xFFFFFFFF,
+]
+INVALID_UINT32 = [
+    0xFFFFFFFFF,
+    -1,
+]
+VALID_UINT64 = [
+    0x00,
+    0x01,
+    0x0123456789ABCDEF,
+    0xFFFFFFFFFFFFFFFF,
+]
+INVALID_UINT64 = [
+    0xFFFFFFFFFFFFFFFFF,
+    -1,
+]
+VALID_STRING = [
+    b'test string',
+]
+INVALID_STRING = [
+    [],
+]
+# See https://datatracker.ietf.org/doc/html/rfc4251#section-5 for examples source
+VALID_MPINT = [
+    0x00,
+    0x9a378f9b2e332a7,
+    0x80,
+    -0x1234,
+    -0xdeadbeef,
+    # Additional large int test
+    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+]
+INVALID_MPINT = [
+    [],
+]
+
+
+def test_parse_openssh_version():
+    assert parse_openssh_version(SSH_VERSION_STRING) == SSH_VERSION_NUMBER
+
+
+@pytest.mark.parametrize("boolean", VALID_BOOLEAN)
+def test_valid_boolean(boolean):
+    writer = OpensshWriter()
+    writer.boolean(boolean)
+    assert OpensshParser(writer.bytes()).boolean() == boolean
+
+
+@pytest.mark.parametrize("boolean", INVALID_BOOLEAN)
+def test_invalid_boolean(boolean):
+    result = False
+
+    try:
+        OpensshWriter().boolean(boolean)
+    except TypeError:
+        result = True
+    assert result
+
+
+@pytest.mark.parametrize("uint32", VALID_UINT32)
+def test_valid_uint32(uint32):
+    writer = OpensshWriter()
+    writer.uint32(uint32)
+    assert OpensshParser(writer.bytes()).uint32() == uint32
+
+
+@pytest.mark.parametrize("uint32", INVALID_UINT32)
+def test_invalid_uint32(uint32):
+    result = False
+
+    try:
+        OpensshWriter().uint32(uint32)
+    except (TypeError, ValueError):
+        result = True
+    assert result
+
+
+@pytest.mark.parametrize("uint64", VALID_UINT64)
+def test_valid_uint64(uint64):
+    writer = OpensshWriter()
+    writer.uint64(uint64)
+    assert OpensshParser(writer.bytes()).uint64() == uint64
+
+
+@pytest.mark.parametrize("uint64", INVALID_UINT64)
+def test_invalid_uint64(uint64):
+    result = False
+
+    try:
+        OpensshWriter().uint64(uint64)
+    except (TypeError, ValueError):
+        result = True
+    assert result
+
+
+@pytest.mark.parametrize("ssh_string", VALID_STRING)
+def test_valid_string(ssh_string):
+    writer = OpensshWriter()
+    writer.string(ssh_string)
+    assert OpensshParser(writer.bytes()).string() == ssh_string
+
+
+@pytest.mark.parametrize("ssh_string", INVALID_STRING)
+def test_invalid_string(ssh_string):
+    result = False
+
+    try:
+        OpensshWriter().string(ssh_string)
+    except TypeError:
+        result = True
+    assert result
+
+
+@pytest.mark.parametrize("mpint", VALID_MPINT)
+def test_valid_mpint(mpint):
+    writer = OpensshWriter()
+    writer.mpint(mpint)
+    assert OpensshParser(writer.bytes()).mpint() == mpint
+
+
+@pytest.mark.parametrize("mpint", INVALID_MPINT)
+def test_invalid_mpint(mpint):
+    result = False
+
+    try:
+        OpensshWriter().mpint(mpint)
+    except TypeError:
+        result = True
+    assert result
+
+
+def test_valid_seek():
+    buffer = bytearray(b'buffer')
+    parser = OpensshParser(buffer)
+    parser.seek(len(buffer))
+    assert parser.remaining_bytes() == 0
+    parser.seek(-len(buffer))
+    assert parser.remaining_bytes() == len(buffer)
+
+
+def test_invalid_seek():
+    result = False
+    buffer = b'buffer'
+    parser = OpensshParser(buffer)
+    try:
+        parser.seek(len(buffer) + 1)
+    except ValueError:
+        result = True
+    try:
+        parser.seek(-1)
+        result = False
+    except ValueError:
+        pass
+    assert result
+
+
+def test_writer_bytes():
+    buffer = bytearray(b'buffer')
+    writer = OpensshWriter(buffer)
+
+    assert writer.bytes() == buffer
