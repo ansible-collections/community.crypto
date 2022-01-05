9b9b759cf2dd8940574a9e173f0c98081b1b6574
Felix Fontein <felix@fontein.de>
2022-01-05 17:59:11 +0100

    Fix copyright lines.

--- plugins/doc_fragments/module_privatekey_convert.py
+++ plugins/doc_fragments/module_privatekey_convert.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2016, Yanis Guenane <yanis+ansible@guenane.org>
+# Copyright: (c) 2022, Felix Fontein <felix@fontein.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
