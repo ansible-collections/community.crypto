c73d2170cc977fb9ef8e01b8628af1a3ed3314ff
Jochen Korge <jochen.korge@pcsm.de>
2022-01-18 17:28:14 +0100

    corrected styling

--- plugins/modules/openssh_cert.py
+++ plugins/modules/openssh_cert.py
@@ -111,7 +111,8 @@ options:
             - "The point in time the certificate is valid from. Time can be specified either as relative time or as absolute timestamp.
                Time will always be interpreted as UTC. Valid formats are: C([+-]timespec | YYYY-MM-DD | YYYY-MM-DDTHH:MM:SS | YYYY-MM-DD HH:MM:SS | always)
                where timespec can be an integer + C([w | d | h | m | s]) (for example C(+32w1d2h)).
-               Note that if using relative time this module is NOT idempotent. To ignore this value during comparison with an existing certificate set I(ignore_timestamps=true)."
+               Note that if using relative time this module is NOT idempotent. To ignore this value during comparison with an existing certificate set 
+               I(ignore_timestamps=true)."
             - Required if I(state) is C(present).
         type: str
     valid_to:
@@ -119,7 +120,8 @@ options:
             - "The point in time the certificate is valid to. Time can be specified either as relative time or as absolute timestamp.
                Time will always be interpreted as UTC. Valid formats are: C([+-]timespec | YYYY-MM-DD | YYYY-MM-DDTHH:MM:SS | YYYY-MM-DD HH:MM:SS | forever)
                where timespec can be an integer + C([w | d | h | m | s]) (for example C(+32w1d2h)).
-               Note that if using relative time this module is NOT idempotent. To ignore this value during comparison with an existing certificate set I(ignore_timestamps=true)."
+               Note that if using relative time this module is NOT idempotent. To ignore this value during comparison with an existing certificate set 
+               I(ignore_timestamps=true)."
             - Required if I(state) is C(present).
         type: str
     valid_at:
@@ -414,7 +416,7 @@ class Certificate(OpensshModule):
 
         if self.ignore_timestamps:
             return original_time_parameters.within_range(self.valid_at)
-        
+
         return all([
             original_time_parameters == self.time_parameters,
             original_time_parameters.within_range(self.valid_at)
