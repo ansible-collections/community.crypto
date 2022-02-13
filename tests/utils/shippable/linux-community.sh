6ff0e11a07aed494b68ecc7a897d2b846d5ac29e
Felix Fontein <felix@fontein.de>
2022-02-13 22:28:43 +0100

    Adjust tests for new operating systems, and pass on Python version as well.

--- tests/utils/shippable/linux-community.sh
+++ tests/utils/shippable/linux-community.sh
@@ -6,13 +6,14 @@ declare -a args
 IFS='/:' read -ra args <<< "$1"
 
 image="${args[1]}"
+python="${args[2]}"
 
-if [ "${#args[@]}" -gt 2 ]; then
-    target="shippable/posix/group${args[2]}/"
+if [ "${#args[@]}" -gt 3 ]; then
+    target="shippable/posix/group${args[3]}/"
 else
     target="shippable/posix/"
 fi
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
-    --docker "quay.io/ansible-community/test-image:${image}"
+    --docker "quay.io/ansible-community/test-image:${image}" --python "${python}"
