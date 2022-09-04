152c5422f1bfce18392dd8d33c54aaa17b5a661c
Maxwell G <9920591+gotmax23@users.noreply.github.com>
2022-09-04 15:21:31 -0500

    Prefer unitest.mock by universally using compat.mock (#506)
    * tests.unit.compat.mock: Remove legacy compat code
    
    This removes old Python 3.4 compatibility code that is no longer needed.
    
    * Prefer unitest.mock by universally using compat.mock
    
    `mock` is a backport of the `unittest.mock` module from the stdlib, and
    there's no reason to use it on newer Python versions.

--- tests/unit/plugins/module_utils/acme/test_io.py
+++ tests/unit/plugins/module_utils/acme/test_io.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-from mock import MagicMock
+from ansible_collections.community.crypto.tests.unit.compat.mock import MagicMock
 
 
 from ansible_collections.community.crypto.plugins.module_utils.acme.io import (
