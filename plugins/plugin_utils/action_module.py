cac596038636cb0bed84b29b17436658383a1ece
Felix Fontein <felix@fontein.de>
2022-09-22 21:55:55 +0200

    Only access C.STRING_CONVERSION_ACTION for old ansible-base / Ansible versions.

--- plugins/plugin_utils/action_module.py
+++ plugins/plugin_utils/action_module.py
@@ -116,7 +116,6 @@ class AnsibleActionModule(object):
         self.required_by = required_by
         self._diff = self.__action_plugin._play_context.diff
         self._verbosity = self.__action_plugin._display.verbosity
-        self._string_conversion_action = C.STRING_CONVERSION_ACTION
 
         self.aliases = {}
         self._legal_inputs = []
@@ -445,7 +444,7 @@ class AnsibleActionModule(object):
         }
 
         # Ignore, warn, or error when converting to a string.
-        allow_conversion = opts.get(self._string_conversion_action, True)
+        allow_conversion = opts.get(C.STRING_CONVERSION_ACTION, True)
         try:
             return check_type_str(value, allow_conversion)
         except TypeError:
@@ -460,10 +459,10 @@ class AnsibleActionModule(object):
                 from_msg = '{0}: {1!r}'.format(param, value)
                 to_msg = '{0}: {1!r}'.format(param, to_text(value))
 
-            if self._string_conversion_action == 'error':
+            if C.STRING_CONVERSION_ACTION == 'error':
                 msg = common_msg.capitalize()
                 raise TypeError(to_native(msg))
-            elif self._string_conversion_action == 'warn':
+            elif C.STRING_CONVERSION_ACTION == 'warn':
                 msg = ('The value "{0}" (type {1.__class__.__name__}) was converted to "{2}" (type string). '
                        'If this does not look like what you expect, {3}').format(from_msg, value, to_msg, common_msg)
                 self.warn(to_native(msg))
