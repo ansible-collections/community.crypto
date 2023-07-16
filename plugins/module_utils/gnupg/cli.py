be8c503a24d7265e89851431dc6744f22bfdd71f
Felix Fontein <felix@fontein.de>
2023-07-16 16:53:03 +0200

    Do not create a keyring when there isn't one.

--- plugins/module_utils/gnupg/cli.py
+++ plugins/module_utils/gnupg/cli.py
@@ -45,5 +45,5 @@ def get_fingerprint_from_stdout(stdout):
 def get_fingerprint_from_file(gpg_runner, path):
     if not os.path.exists(path):
         raise GPGError('{path} does not exist'.format(path=path))
-    stdout = gpg_runner.run_command(['--with-colons', '--import-options', 'show-only', '--import', path], check_rc=True)[1]
+    stdout = gpg_runner.run_command(['--no-keyring', '--with-colons', '--import-options', 'show-only', '--import', path], check_rc=True)[1]
     return get_fingerprint_from_stdout(stdout)
