434d8514ab6a815aa56e6a6cd3d8c701a1895f70
Steffen Gufler <s.gufler@online.de>
2024-02-10 13:22:43 +0100

    luks_device: Fix testing of LUKS passphrases when only testing one key slot

--- plugins/modules/luks_device.py
+++ plugins/modules/luks_device.py
@@ -799,6 +799,11 @@ class CryptHandler(Handler):
             if 'No usable keyslot is available.' in result[output]:
                 return False
 
+        # This check is necessary due to cryptsetup in version 2.0.3 not printing 'No usable keyslot is available'
+        # when using the --key-slot parameter in combination with --test-passphrase
+        if result[RETURN_CODE] == 1 and keyslot is not None and result[STDOUT] == '' and result[STDERR] == '':
+            return True
+
         raise ValueError('Error while testing whether keyslot exists on %s: %s'
                          % (device, result[STDERR]))
 
