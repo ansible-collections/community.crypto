3a3bb86b959f9bbf2de04c622a0984c3aeb701bb
dlehrman <dlehrman@users.noreply.github.com>
2024-07-07 15:04:35 -0400

    Include value in fail output for wrong data type
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/get_certificate.py
+++ plugins/modules/get_certificate.py
@@ -438,7 +438,7 @@ def main():
                         tls_ctx_option_int = tls_ctx_option
                     # If the item is not a string nor integer
                     else:
-                        module.fail_json(msg="tls_ctx_options must be a string or integer")
+                        module.fail_json(msg="tls_ctx_options must be a string or integer, got {0!r}".format(tls_ctx_option))
 
                     try:
                         # Add the int value of the item to ctx options
