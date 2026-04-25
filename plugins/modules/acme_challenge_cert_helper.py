3a9adb9632d95de9fb37ce958164d896f62ef7c8
Felix Fontein <felix@fontein.de>
2026-04-25 13:25:48 +0200

    Fix sorting.

--- plugins/modules/acme_challenge_cert_helper.py
+++ plugins/modules/acme_challenge_cert_helper.py
@@ -177,8 +177,8 @@ try:
     import cryptography.hazmat.backends
     import cryptography.hazmat.primitives.asymmetric.dsa
     import cryptography.hazmat.primitives.asymmetric.ec
-    import cryptography.hazmat.primitives.asymmetric.ed25519
     import cryptography.hazmat.primitives.asymmetric.ed448
+    import cryptography.hazmat.primitives.asymmetric.ed25519
     import cryptography.hazmat.primitives.asymmetric.padding
     import cryptography.hazmat.primitives.asymmetric.rsa
     import cryptography.hazmat.primitives.asymmetric.utils
